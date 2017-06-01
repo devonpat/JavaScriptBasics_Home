e18ad2eea913cc63fa24d9dd02e93e9362d4f9eb
Devon Patey <devon.patey@gmail.com>
2017-06-01 17:24:16 -0600

    homework

---
+++ jsBasic.js
@@ -0,0 +1,78 @@
+// Step 1
+var a = 100;
+var b = 200;
+console.log(a + b);
+
+
+/*
+// step 2
+var cars = ['ferrari', 'porsche', 'mustang'];
+var animals = ['cat', 'dog', 'lemur'];
+var things = [cars, animals]
+console.log(cars[0])
+console.log(animals[1])
+*/
+
+
+/*
+// step 3 & 4
+function high(number){
+	if (number < 100) {
+		alert ('lesser than 100');
+	}else {
+		alert ('greater than 100');
+	}
+}
+high(110);
+*/
+
+
+/*
+// step 5
+var list = "exact";
+function string(Devon){
+	if (Devon === list) {
+		console.log('same');
+	}else {
+		console.log('not same');
+	}
+}
+
+string("exact")
+*/
+
+
+/*
+// step 6
+function printName(name){
+	console.log(name)
+}
+
+printName("Devon");
+*/
+
+
+/*
+// step 7
+function print() {
+	console.log('print something')
+}
+
+print()
+*/
+
+
+/*
+// step 8
+function door(whichDoor) {
+	if (whichDoor === 1) {
+		console.log('new car');
+	}else if (whichDoor === 2) {
+		console.log('new house');
+	}else {
+		console.log('new shoes');
+	}
+}
+
+door(2)
+*/
